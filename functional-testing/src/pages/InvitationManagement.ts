515809cd201d7fd53f10cdb86ff2266534a0fb8f
jhojasiRA <jhojasi.sierra@rockwellautomation.com>
2022-03-14 15:37:30 -0500

    invitationManagement page migrated

---
+++ functional-testing/src/pages/InvitationManagement.ts
@@ -0,0 +1,18 @@
+import {Action} from '../globalTasks/Action';
+
+export class Organization extends Action {
+    get resendButton() { return browser.$('//*[@role="row"]/child::*[contains(text(),"Active")]/following::*[contains(text(),"RESEND")]');}
+    get cancelButton() { return browser.$('//*[@role="row"]/child::*[contains(text(),"Active")]/following::*[contains(text(),"CANCEL")]');}
+    get okButton() { return browser.$('//*[contains(@id, "mat-dialog")]//button');}
+
+    public async resendActiveInvitation(): Promise<void> {
+        await this.click(this.resendButton);
+        await this.click(this.okButton);
+      }
+    
+      public async cancelActiveInvitation(): Promise<void> {
+        await this.click(this.cancelButton);
+        await this.click(this.okButton);
+      }
+
+}
