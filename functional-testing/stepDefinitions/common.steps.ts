2228f1fa3b932aa10c663369379b77d5911bb73b
jhojasiRA <jhojasi.sierra@rockwellautomation.com>
2022-03-18 10:15:57 -0500

    Step definition: Access Management migrated

--- functional-testing/stepDefinitions/common.steps.ts
+++
@@ -1,7 +0,0 @@
-import { Given } from '@wdio/cucumber-framework';
-import { indexPage } from '../support/Hooks'
-
-Given(/^the user access the portal URL$/, async() => {
-    await indexPage.open('')
-    await indexPage.clickSignin();
-})
