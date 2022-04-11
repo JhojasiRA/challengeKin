67a65ce9f64a2d40f1516710ede59fea58f741b6
Jorge Zapata Cortinez <JorgeIvan.Zapata@rockwellautomation.com>
2022-04-11 16:21:15 -0500

    tags syntheticTrx

--- functional-testing/config/wdio.conf.ts
+++ functional-testing/config/wdio.conf.ts
@@ -9,17 +9,28 @@ intStepsTimeout = isNaN(global.intElementsTimeout) ? intStepsTimeoutDefault : in
 export const BaseConfig: WebdriverIO.Config = {
 
     specs: [
+        [
+            'functional-testing/features/syntheticTrx/common_services_login.feature',
+            'functional-testing/features/syntheticTrx/create_organization.feature',
+            'functional-testing/features/syntheticTrx/join_request.feature',
+            'functional-testing/features/syntheticTrx/access_management.feature',
+            'functional-testingfeatures/syntheticTrx/launch_services.feature',
+            'functional-testingfeatures/syntheticTrx/invitations_management.feature'
+        ],
         'functional-testing/features/*/*.feature'
     ],
+
     exclude: [
     ],
+
+
     // host: "localhost",
     // port: 4444,
     // path: '/wd/hub',
     // protocol: 'http',
     // runner: 'local',
 
-    
+
     //
     // ============
     // Capabilities
@@ -44,29 +55,29 @@ export const BaseConfig: WebdriverIO.Config = {
     //
     capabilities: [
         {
-    
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 1,
-        },
-    // {
-    //     maxInstances: 1,
-    //     browserName:'firefox',
-    //     'moz:firefoxOptions':{
-    //         args:['-start-maximized']
-    //     //     args:['-headless','--disable-gpu','--window-size=2000,1024','--disable-infobars','--disable-dev-shm-usage','--no-sandbox']
-    //     }
 
-    // },
-    // {
-    //     maxInstances: 1,
-    //     browserName:'MicrosoftEdge',
-    //     'ms:EdgeOptions':{
-    //         args:['start-maximized']
-    //     //     args:['-headless','--disable-gpu','--window-size=2000,1024','--disable-infobars','--disable-dev-shm-usage','--no-sandbox']
-    //     }
-    // }
+            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+            // grid with only 5 firefox instances available you can make sure that not more than
+            // 5 instances get started at a time.
+            maxInstances: 1,
+        },
+        // {
+        //     maxInstances: 1,
+        //     browserName:'firefox',
+        //     'moz:firefoxOptions':{
+        //         args:['-start-maximized']
+        //     //     args:['-headless','--disable-gpu','--window-size=2000,1024','--disable-infobars','--disable-dev-shm-usage','--no-sandbox']
+        //     }
+
+        // },
+        // {
+        //     maxInstances: 1,
+        //     browserName:'MicrosoftEdge',
+        //     'ms:EdgeOptions':{
+        //         args:['start-maximized']
+        //     //     args:['-headless','--disable-gpu','--window-size=2000,1024','--disable-infobars','--disable-dev-shm-usage','--no-sandbox']
+        //     }
+        // }
     ],
     logLevel: 'error',
     bail: 0,
@@ -74,13 +85,13 @@ export const BaseConfig: WebdriverIO.Config = {
     connectionRetryTimeout: 60000,
     connectionRetryCount: 1,
     framework: 'cucumber',
-    
+
     reporters: [
         "spec",
         ['cucumberjs-json', {
-                jsonFolder: './reports/',
-                language: 'en',
-            },
+            jsonFolder: './reports/',
+            language: 'en',
+        },
         ],
     ],
 
@@ -99,7 +110,7 @@ export const BaseConfig: WebdriverIO.Config = {
         timeout: intStepsTimeout,
         ignoreUndefinedDefinitions: false
     },
-    
+
     /**
      * Gets executed once before all workers get launched.
      * @param {Object} config wdio configuration object
@@ -127,7 +138,7 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {Array.<String>} specs List of spec file paths that are to be run
      * @param {String} cid worker id (e.g. 0-0)
      */
-   
+
     /**
      * Gets executed before test execution begins. At this point you can access to all global
      * variables like `browser`. It is the perfect place to define custom commands.
@@ -141,7 +152,7 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {String} commandName hook command name
      * @param {Array} args arguments that command would receive
      */
- 
+
     /**
      * Cucumber Hooks
      *
@@ -149,20 +160,20 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {String}                   uri      path to feature file
      * @param {GherkinDocument.IFeature} feature  Cucumber feature object
      */
- 
+
     /**
      *
      * Runs before a Cucumber Scenario.
      * @param {ITestCaseHookParameter} world world object containing information on pickle and test step
      */
- 
+
     /**
      *
      * Runs before a Cucumber Step.
      * @param {Pickle.IPickleStep} step     step data
      * @param {IPickle}            scenario scenario pickle
      */
- 
+
     /**
      *
      * Runs after a Cucumber Step.
@@ -173,7 +184,7 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {string}             result.error    error stack if scenario failed
      * @param {number}             result.duration duration of scenario in milliseconds
      */
-   
+
     /**
      *
      * Runs before a Cucumber Scenario.
@@ -184,7 +195,7 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {number}                 result.duration duration of scenario in milliseconds
      */
     //@ts-ignore
-    afterScenario: async (world,result) => {
+    afterScenario: async (world, result) => {
         await browser.reloadSession();
     },
 
@@ -196,7 +207,7 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {GherkinDocument.IFeature} feature  Cucumber feature object
      */
 
-    
+
     /**
      * Runs after a WebdriverIO command gets executed
      * @param {String} commandName hook command name
@@ -204,7 +215,7 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {Number} result 0 - command success, 1 - command error
      * @param {Object} error error object if any
      */
-  
+
     /**
      * Gets executed after all tests are done. You still have access to all global variables from
      * the test.
@@ -212,7 +223,7 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
- 
+
     /**
      * Gets executed right after terminating the webdriver session.
      * @param {Object} config wdio configuration object
@@ -229,25 +240,25 @@ export const BaseConfig: WebdriverIO.Config = {
      * @param {<Object>} results object containing test results
      */
     //@ts-ignore
-    onComplete: async() => {
+    onComplete: async () => {
         const path = require('path')
         //@ts-ignore
         // TODO: Set test type from execution
-        await logJSONresults(path.join(process.cwd(),`reports`), `WDIO Template`, new Date().toUTCString(), process.argv[process.argv.length - 1])
-            .then(() => {console.log(`Splunk logs sent`)})
+        await logJSONresults(path.join(process.cwd(), `reports`), `WDIO Template`, new Date().toUTCString(), process.argv[process.argv.length - 1])
+            .then(() => { console.log(`Splunk logs sent`) })
             .catch((err) => console.log(`Splunk logs were not sent: ${err.message}`));
-        
+
         await generate({
             jsonDir: './reports/',
             reportPath: './reports/',
             // for more options see https://github.com/wswebcreation/multiple-cucumber-html-reporter#options
         });
     }
-    
+
     /**
     * Gets executed when a refresh happens.
     * @param {String} oldSessionId session ID of the old session
     * @param {String} newSessionId session ID of the new session
     */
-   
+
 }
