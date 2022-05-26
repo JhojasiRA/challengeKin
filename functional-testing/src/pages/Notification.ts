8454a5f6ed1f3af2634cfb8da9d90d6397bb0e1a
jhojasiRA <jhojasi.sierra@rockwellautomation.com>
2022-05-26 14:47:29 -0500

    Deleting notifications from panel and viewl all page

--- functional-testing/src/pages/Notification.ts
+++ functional-testing/src/pages/Notification.ts
@@ -2,25 +2,49 @@ import {Action} from '../globalTasks/Action';
 
 export class Notification extends Action {
     get notificacionIcon() { return browser.$('//*[@class= "ra-icon-ide-sm-alarm ra-common-icon"]');}
-    get removeNotificationOption() { return browser.$('(//*[@class="ra-icon-ide-sm-close"])[1]');}
+    get removeNotificationOption() { return browser.$('(//*[@class="mat-tooltip-trigger ra-icon-ide-sm-close"])[1]');}
     get deleteButton() { return browser.$('//*[contains(text(), " DELETE")]');}
-    get removeNotifiationModal() { return browser.$('//*[@id ="mat-dialog-1"]');}
-
-    get clearMessageNotification() { return browser.$('//*[contains(text(), "All Clear")]');}
-    
+    get clearAllPanelNotifications() { return browser.$('//*[contains(text(), " Clear All ")]');}
+    get viewAllButton() { return browser.$('//*[contains(text(), " View All ")]');}
+    get deleteAllNotificationsOption() { return browser.$('//*[contains(text(), "DELETE ALL")]');}
+    get clearButton() { return browser.$('//*[contains(text(), "CLEAR")]');}
+    get trashIcon() { return browser.$('(//*[@class="ra-icon-ide-sm-delete close-notification"])[1]');}
+    get allClearMessage() { return browser.$('//*[contains(text(), "All Clear")]');}
 
     public async icon(): Promise<void> {
         await browser.pause(1000);
         await this.click(this.notificacionIcon);
       }
 
-    public async removeNotification(): Promise<void> {
+    public async removeNotificationFromPanel(): Promise<void> {
         await browser.pause(1000);
         await this.click(this.removeNotificationOption);
-       // await this.removeNotifiationModal.waitForDisplayed({ timeout: global.intElementsTimeout })
         await this.click(this.deleteButton);
-        await browser.pause(2000);
       }
+    public async clearAllNotificationsFromPanel(): Promise<void> {
+        await browser.pause(1000);
+        await this.click(this.clearAllPanelNotifications);
+        await this.click(this.clearButton);
+      }
+    public async deleteAllNotificationsFromViewPage(): Promise<void> {
+        await browser.pause(1000);
+        await this.click(this.viewAllButton);
+        await browser.pause(1000);
+        await this.click(this.deleteAllNotificationsOption);
+        await browser.pause(1000);
+        await this.click(this.clearButton);
+      }
+    public async clearNotificationsFromViewAllPage(): Promise<void> {
+        await browser.pause(1000);
+        await this.click(this.viewAllButton);
+        await browser.pause(1000);
+        await this.click(this.trashIcon);
+        await this.click(this.deleteButton);
+      }
+
+    public getAllClearMessage(): WebdriverIO.Element {
+        return this.allClearMessage;
+    }  
     }
 
 
--- functional-testing/stepDefinitions/syntheticTrx/NotificationStepDefinition.ts
+++ functional-testing/stepDefinitions/syntheticTrx/NotificationStepDefinition.ts
@@ -1,12 +1,29 @@
 import {When, Then } from '@cucumber/cucumber'
-import { notification } from '../../support/Hooks';
+import { notification,question } from '../../support/Hooks';
 
 When('the user goes to the notification option', async() => {
     await notification.icon();
 });
 When('the user removes a notification from the panel', async() => {
-    await notification.removeNotification();
-
+    await notification.removeNotificationFromPanel();
+});
+When('the user clears one notification on the view all page', async() => {
+    await notification.clearNotificationsFromViewAllPage();
 });
 Then('the user will see one notification less on his NavBar', async() => {
   });
+
+When('the user clears all notifications on the panel notification', async() => {
+    await notification.clearAllNotificationsFromPanel();
+});
+Then('The user will see an {string} panel', async(Notification) => {
+    await question.assertElementText(notification.getAllClearMessage(),Notification);
+  });
+
+When('the user clears all notifications on the view all page', async() => {
+    await notification.deleteAllNotificationsFromViewPage();
+});
+Then('The user will see an empty page', async() => {
+    await question.assertElementNotExist(notification.trashIcon);
+  });
+
