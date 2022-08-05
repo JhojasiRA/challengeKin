43aac540f09dacf9c9dcfe212365ddb0d1a246e4
jhojasiRA <jhojasi.sierra@rockwellautomation.com>
2022-08-05 09:23:32 -0500

    defining scenarios, methods and refactoring NotificationStepDefinitions

--- functional-testing/services/NotificationService.ts
+++ functional-testing/services/NotificationService.ts
@@ -1,10 +1,11 @@
 import 'regenerator-runtime/runtime';
 const axios = require('axios');
-import {getToken5, getToken} from '../Token';
+import {getToken5} from '../Token';
+import {creds} from '../constant.json';
 import { toUUID } from 'to-uuid'
 
 export var createNotification = async() =>{
-    let token = await getToken5(process.env.USERNAME, process.env.PASSWORD);
+    let token = await getToken5(creds.user, process.env.PASSWORD);
     try {
         let url = `${process.env.API_CS}/api/tenant`;
         let config = {
@@ -51,109 +52,34 @@ for (let n = 0; n < 11; n++) {
         return error.response.data;
     }
 }
-export var createTenantNotification = async() =>{
-    let token = await getToken();
-    try {
-    let url  = `${process.env.URL_NOTIF}/api/messages/tenant/654afe138843574c8a7cf198cedc04e3`
-    let config = {
-        headers: {
-            'Authorization': 'Bearer ' + token,
-            'Content-Type':'application/json'
-        }
-    }
-    const response = await axios.post(url,{
-            body: "Automation tenant Test message",
-            isEphemeral: true
-    },config);
-    var messageAndStatus = {id:response.data.id,status:response.status};
-    return messageAndStatus;
-    } catch (error) {
-        console.log(error);
-        return error.response.data;
-    }
-}
 
-
-//--------------get messages
 export var getAllNotifications = async() =>{
+    let token = await getToken5(creds.user, process.env.PASSWORD);
     try {
     let url  = `${process.env.URL_NOTIF}/api/messages`
     let config = {
         headers: {
-            'Authorization': 'Bearer ' + global.accessToken,
+            'Authorization': 'Bearer ' + token,
         }
     }
     const response = await axios.get(url,config);
-    var count = 0;
-    var flag = false;
-     while (flag == false) {
-        var userInfo = {status:response.status,Message:response.data[count].body,role:response.data[count].role};
-        if (userInfo.Message == 'Your have been granted access to test automation tenant') {
-            flag = true;
-        }
-        if(userInfo.Message == 'Invitation accepted by Tester4 User for accessing Vault' && userInfo.role == 'Admin'){
-            flag = true;
-        }
-
-        else {
-            count++;
-        }
-     }
-    return userInfo;
+    return response.status;
     } catch (error) {
         return error.response.status;
     }
 }
 
-
-export var getMessageByTenantId = async() =>{
-    let msgtenantId = await createTenantNotification();
+export var getMessageById = async() =>{
+    let token = await getToken5(creds.user, process.env.PASSWORD);
+    let msgtenantId = await createNotification();
     try {
     let url  = `${process.env.URL_NOTIF}/api/messages/${msgtenantId.id}`
     let config = {
         headers: {
-            'Authorization': 'Bearer ' + global.accessToken,
+            'Authorization': 'Bearer ' + token,
         }
     }
     const response = await axios.get(url,config);
-    return response.status;
-    } catch (error) {
-        console.log(error);
-        return error.response.data;
-    }
-}
-
-
-export var patchAllProperties = async() =>{
-    try {
-    let url  = `${process.env.URL_NOTIF}/api/messages`
-    let config = {
-        headers: {
-            'Authorization': 'Bearer ' + global.accessToken,
-            'Content-Type':'application/json'
-        }
-    }
-    const response = await axios.patch(url,{
-        isRead: true
-      },config);
-    return response.status;
-    } catch (error) {
-        console.log(error);
-        return error.response.data;
-    }
-}
-//Deletes
-
-export var deleteByMessageId = async() =>{
-    let msgtenantId = await createTenantNotification();
-    try {
-    let url  = `${process.env.URL_NOTIF}/api/messages/${msgtenantId.id}`
-    let config = {
-        headers: {
-            'Authorization': 'Bearer ' + global.accessToken,
-        }
-    }
-    const response = await axios.delete(url,config);
     return response.status;
     } catch (error) {
         console.log(error);
@@ -170,9 +96,6 @@ export var deleteAllMessages = async() =>{
         }
     }
     const response = await axios.delete(url,config);
-    console.log("Delete ALL messages");
-    console.log(response.data);
-    console.log(response.status);
     return response.status;
     } catch (error) {
         console.log(error);
--- functional-testing/stepDefinitions/back/NotificationService.ts
+++ functional-testing/stepDefinitions/back/NotificationService.ts
@@ -1,6 +1,6 @@
 import {When, Then } from '@cucumber/cucumber';
 import {assert} from 'chai'; 
-import {createUserNotification,createTenantNotification,getAllNotifications,getMessageByUserId,getMessageByTenantId,patchReadProperty,patchAllProperties,deleteByMessageId} from '../../services/NotificationService';
+import {createNotification,getAllNotifications,getMessageById,deleteAllMessages} from '../../services/NotificationService';
 
 
     When('it sends a POST request to create a user notification {string}', async(endpoint) => {
@@ -8,18 +8,10 @@ import {createUserNotification,createTenantNotification,getAllNotifications,getM
     });
   
     Then('The POST request user notification should be: status {string} ok', async(status) => {
-        let statusUser = await createUserNotification(); 
+        let statusUser = await createNotification(); 
         assert.equal(status,statusUser.status);
     });
   
-    When('it sends a POST request to create a tenant notification {string}', async(endpoint) => {
-        console.log(endpoint);
-    });
-  
-    Then('The POST request tenant notification should be: status {string} ok', async(status) => {
-        let statusTenant = await createTenantNotification(); 
-        assert.equal(status,statusTenant.status);
-    });
 //------------------Notifications access request and invitations-------------------------
     When('It sends a GET request to get all user messages {string}', async(endpoint) => {
         console.log(endpoint);
@@ -39,42 +31,15 @@ import {createUserNotification,createTenantNotification,getAllNotifications,getM
     });
   
     Then('The GET request to a specific user message should be: status {string} ok', async(status) => {
-        let statusNotifications = await getMessageByUserId(); 
+        let statusNotifications = await getMessageById(); 
         assert.equal(status,statusNotifications);
     });
- 
-    When('it sends a GET request to get a specific tenant message {string}', async(endpoint) => {
-        console.log(endpoint);
-    });
-  
-    Then('The GET request to a specific tenant message should be: status {string} ok', async(status) => {
-        let statusNotifications = await getMessageByTenantId(); 
-        assert.equal(status,statusNotifications);
-    });
-
-    When('it send a PATCH request to update read property of a message {string}', async(endpoint) => {
-        console.log(endpoint);
-    });
-  
-    Then('The PACTH request should be: status {string} ok', async(status) => {
-        let statusPropertie = await patchReadProperty(); 
-        assert.equal(status,statusPropertie);
-    });
-
-    When('it send a PATCH request to update read property {string}', async(endpoint) => {
-        console.log(endpoint);
-    });
-  
-    Then('The PACTH request of messages should be: status {string} ok', async(status) => {
-        let statusProperties = await patchAllProperties(); 
-        assert.equal(status,statusProperties);
-    });
-  
+//----------------------------
     When('it send erase request {string}', async(endpoint) => {
         console.log(endpoint);
     });
   
     Then('The message request Should be: status {string} ok', async(status) => {
-        let statusProperties = await deleteByMessageId(); 
+        let statusProperties = await deleteAllMessages(); 
         assert.equal(status,statusProperties);
     });
