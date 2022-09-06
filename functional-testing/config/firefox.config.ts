9d05f0fa87e89377482405cb23474b1b3fffb9ec
jhojasiRA <jhojasi.sierra@rockwellautomation.com>
2022-09-06 16:50:53 -0500

    resolving firefox conflict

--- functional-testing/config/firefox.config.ts
+++ functional-testing/config/firefox.config.ts
@@ -1,23 +1,21 @@
 import { BaseConfig } from "./wdio.conf"
 
+
 const FirefoxConfig = {
-    
+    path: '/wd/hub',
     hostname: process.env.HOSTNAME,
     port: parseInt(process.env.PORT),
-    path: '/wd/hub',
     protocol: 'http',
     capabilities: [{​
         maxInstances: parseInt(process.env.MAX_INSTANCES),
         browserName: 'firefox',
         acceptInsecureCerts : true,
         'moz:firefoxOptions': {
-            // One 'args' should be always commented, args: [] is mainly used locally for debugging
-            // args: ['-headless'],
-            args: ['--disable-gpu','--disable-popup-blocking', '--no-default-browser-check','--no-sandbox', 'start-maximized'],
-
-            prefs: { credentials_enable_service: false }
+            args: [],
+            prefs: { credentials_enable_service: false
+            }
         }
-    }]
+    },]
 }
 
 
