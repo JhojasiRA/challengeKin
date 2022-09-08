fed1bc2ca7354438a849918c9f34a4fd4c0a3884
jhojasiRA <jhojasi.sierra@rockwellautomation.com>
2022-09-08 14:44:00 -0500

    invitations.feature refactored

--- functional-testing/src/pages/InvitationManagement.ts
+++ functional-testing/src/pages/InvitationManagement.ts
@@ -1,9 +1,12 @@
 import {Action} from '../globalTasks/Action';
 
 export class InvitationManagement extends Action {
-    get resendButton() { return browser.$('//*[@role="row"]/child::*[contains(text(),"Active")]/following::*[contains(text(),"RESEND")]');}
-    get cancelButton() { return browser.$('//*[@role="row"]/child::*[contains(text(),"Active")]/following::*[contains(text(),"delete")]');}
+    get resendButton() { return browser.$('//*[@role="row"]/child::*[contains(text(),"Active")]/following::*[contains(text(),"Resend")]');}
+    //get cancelButton() { return browser.$('//*[@role="row"]/child::*[contains(text(),"Active")]/following::*[contains(text(),"delete")]');}
+    get cancelButton() { return browser.$('//*[@class="ag-remove-invitation-container ra-common-icon"]');}
     get okButton() { return browser.$('//*[contains(@id, "mat-dialog")]//button');}
+    get DeleteButton() { return browser.$('//*[contains(text(),"Delete")]');}
+    
 
     public async resendActiveInvitation(): Promise<void> {
         await browser.pause(1000);
@@ -14,7 +17,7 @@ export class InvitationManagement extends Action {
       public async cancelActiveInvitation(): Promise<void> {
         await browser.pause(1000);
         await this.click(this.cancelButton);
-        await this.click(this.okButton);
+        await this.click(this.DeleteButton);
       }
 
 }
