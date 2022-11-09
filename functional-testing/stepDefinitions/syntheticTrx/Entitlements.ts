74febeac7e2e7992f7f1158b2743a73256b19e5c
Jorge Zapata Cortinez <JorgeIvan.Zapata@rockwellautomation.com>
2022-11-09 17:57:42 -0500

    merge conflicts

--- functional-testing/stepDefinitions/syntheticTrx/Entitlements.ts
+++ functional-testing/stepDefinitions/syntheticTrx/Entitlements.ts
@@ -38,7 +38,7 @@ When('user tries to purchase another {string} entitlement', async (entitlement: 
   await browser.pause(1000);
   await  menuhomepage.entitlementsOption();
   await entitlements.purchase(entitlement);
-  await entitlements.allocateEnt();
+  await entitlements.allocateEntitlement(null);
 });
 
 Then('user should see {string} message', async (allocatedMessage: string) =>{
